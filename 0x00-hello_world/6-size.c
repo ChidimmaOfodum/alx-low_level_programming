a3cd9c6fc34fa6e3b3c76f4ac255897251451ddf
Chidimma Ofodum <chidimmamofodum@gmail.com>
2023-04-21 00:23:31 -0700

    printing multiple lines of code

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints multiple lines of code
+ * Return: 0
+*/
+
+int main(void)
+{
+	printf("Size of a char: 1 byte(s)\n");
+	printf("Size of a int: 4 byte(s)\n");
+	printf("Size of a long int: 4 byte(s)\n");
+	printf("Size of a char: 1 byte(s)\n");
+	return (0);
+}
