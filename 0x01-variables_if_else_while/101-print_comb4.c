67b846b646444ad65a832732f95c1e386eb60413
Chidimma Ofodum <chidimmamofodum@gmail.com>
2023-04-22 03:56:06 -0700

    printing combination of 3 digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/*
+ * main - Entry point
+ * Description: printing combination of three digits
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (j = i + 1; j < 58; j++)
+		{
+			for (k = j + 1; j < 58; j++)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				if (k == 57 && j == 56 && i == 55)
+				{
+					putchar('\n');
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+
+				}
+			}
+		}
+	}
+}
