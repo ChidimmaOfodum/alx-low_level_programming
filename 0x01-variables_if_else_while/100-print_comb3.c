8205646ac95d35d6b94460ddbdbfd1c09b8a83b8
Chidimma Ofodum <chidimmamofodum@gmail.com>
2023-04-22 03:44:39 -0700

    combination of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: prints combination of numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (j = i + 1; j < 58; j++)
+		{
+			putchar(i);
+			putchar(j);
+			if (i == 56 && j == 57)
+			{
+				putchar('\n');
+			}
+			else
+			{
+			putchar(',');
+			}
+		}
+	}
+
+	return (0);
+}
