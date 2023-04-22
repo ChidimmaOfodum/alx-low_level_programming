982e0739b87a7bddc04c44899fe38bdcc337f55e
Chidimma Ofodum <chidimmamofodum@gmail.com>
2023-04-21 17:57:21 -0700

    printing single digits of base 10 numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: printing single digits of base 10 integers
+ * Return:0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10 ; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+
+	return (0);
+}
