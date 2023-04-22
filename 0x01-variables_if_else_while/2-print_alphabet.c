8a99a293dc8f6b09e9a88fe3f4610fe1eee0ec98
Chidimma Ofodum <chidimmamofodum@gmail.com>
2023-04-21 17:10:33 -0700

    printing all characters of alphabets with a for loop

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description :prints all lowercase letters of alphabets
+ * Return: 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+
+	return (0);
+}
