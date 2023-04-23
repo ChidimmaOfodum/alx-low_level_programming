f2dba89771e7f88aea32b3cba9d24bf01cde68da
Chidimma Ofodum <chidimmamofodum@gmail.com>
2023-04-23 10:31:32 -0700

    betty coding styles while printing to standard output

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
+/**
+ * main - Entry point
+ * Description: a program that outputs a line to output
+ * Return: 1
+ */
 
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stderr);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+		stderr);
 	return (1);
 }
