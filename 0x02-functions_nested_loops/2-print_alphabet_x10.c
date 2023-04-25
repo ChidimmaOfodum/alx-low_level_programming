32b5eb2efae8bb0367a18e4ab54f560edd28855e
Chidimma Ofodum <chidimmamofodum@gmail.com>
2023-04-25 14:59:06 -0700

    printing alphabets 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main -Entry point
+ * Description - a program that prints alphabets 10 times
+ * Return : 0
+ */
+
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,3 +9,18 @@
                 }
                 putchar('\n');
         }
+
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			putchar(j);
+		}
+		putchar('\n');
+	}
+}
