05daf34d40a147d072b855c3679ff7c251966224
Chidimma Ofodum <chidimmamofodum@gmail.com>
2023-04-25 14:48:53 -0700

    moving functions to main.h

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 /**
  * main - Entry point
  * Descrption: a program that calls a function
@@ -7,15 +8,5 @@
 
 int main(void)
 {
-	void print_alphabet(void)
-	{
-		char i;
-
-		for (i = 'a'; i <= 'z'; i++)
-		{
-			putchar(i);
-		}
-		putchar('\n');
-	}
 	print_alphabet();
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,2 +1,11 @@
 #include <stdio.h>
+ void print_alphabet(void)
+        {
+                char i;
 
+                for (i = 'a'; i <= 'z'; i++)
+                {
+                        putchar(i);
+                }
+                putchar('\n');
+        }
