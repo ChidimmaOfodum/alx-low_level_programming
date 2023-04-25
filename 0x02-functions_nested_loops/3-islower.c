ab7a9cbb9f3c0b49915ef02cfc0232b95fb515e1
Chidimma Ofodum <chidimmamofodum@gmail.com>
2023-04-25 16:36:02 -0700

    checks whether an ascii character is upper or lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _islower- Entry point
+ * @c: describe the parameter
+ * Return: 1 if lowercase otherwise 0
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
