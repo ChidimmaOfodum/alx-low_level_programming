955a733376205d0e891a0679771003773ce75ac6
Chidimma Ofodum <chidimmamofodum@gmail.com>
2023-04-25 15:51:01 -0700

    using _putchar instead of putchar

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -12,8 +12,8 @@ void print_alphabet(void)
 
                 for (i = 'a'; i <= 'z'; i++)
                 {
-                        putchar(i);
+                        _putchar(i);
                 }
-                putchar('\n');
+                _putchar('\n');
         }
 }
