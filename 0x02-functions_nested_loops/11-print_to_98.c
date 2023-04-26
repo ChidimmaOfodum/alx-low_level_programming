528a484f607a5a0b7f0f0dd416d2cc8d39b9a6a6
Chidimma Ofodum <chidimmamofodum@gmail.com>
2023-04-26 14:05:04 -0700

    fixing errors

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -18,25 +18,25 @@ void print_to_98(int n)
 		{
 			if (i == 98)
 			{
-				printf("\n");
+				printf("%d\n", i);
 			}
 			else
 			{
-				printf("%d, ", n);
+				printf("%d, ", i);
 			}
 		}
 	}
 	else
 	{
-		for (i = n; i <= 98; i++)
+		for (i = n; i >= 98; i--)
 		{
 			if (i == 98)
 			{
-				printf("\n");
+				printf("%d\n", i);
 			}
 			else
 			{
-				printf("%d, ", n);
+				printf("%d, ", i);
 			}
 		}
 	}
