0143c6ee6a08bd56008e2422df5218c32d34e224
Chidimma Ofodum <chidimmamofodum@gmail.com>
2023-04-26 13:29:30 -0700

    a program that adds two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - Entry point
+ * @a: first number
+ * @b: second number
+ * Description: a program that adds two numbers
+ * Return: added value
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
