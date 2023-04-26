2b136a4f7dbe2071d035e17846842ec813a185f6
Chidimma Ofodum <chidimmamofodum@gmail.com>
2023-04-26 13:54:56 -0700

    n can be greater or less thann 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,15 +12,32 @@ void print_to_98(int n)
 {
 	int i;
 
-	for (i = n; i <= 98; i++)
+	if (n < 98)
 	{
-		if (n == 98)
+		for (i = n; i <= 98; i++)
 		{
-			printf("\n");
+			if (i == 98)
+			{
+				printf("\n");
+			}
+			else
+			{
+				printf("%d, ", n);
+			}
 		}
-		else
+	}
+	else
+	{
+		for (i = n; i <= 98; i++)
 		{
-			printf("%d, ", n);
+			if (i == 98)
+			{
+				printf("\n");
+			}
+			else
+			{
+				printf("%d, ", n);
+			}
 		}
 	}
 }
