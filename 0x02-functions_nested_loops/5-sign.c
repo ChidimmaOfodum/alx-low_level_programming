a2d9490ddf885429d416354a925aec5f00cec91c
Chidimma Ofodum <chidimmamofodum@gmail.com>
2023-04-25 17:18:30 -0700

    absolute value of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,10 @@
 #include "main.h"
+/**
+ * print_sign - Entry point
+ * Description: prints sign of a number
+ * @n: a number
+ * Return: 0 if zero, 1 if negative and -1 otherwise
+ */
 
 int print_sign(int n)
 {
