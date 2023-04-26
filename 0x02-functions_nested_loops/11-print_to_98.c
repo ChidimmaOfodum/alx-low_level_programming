4068c9c7fb084749444a775451481df2a8f9841b
Chidimma Ofodum <chidimmamofodum@gmail.com>
2023-04-26 13:39:53 -0700

    printing from n to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - Entry point
+ * Description: a program that prints to 98
+ * @n: starting point
+ * Return: 0
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	for (i = n; i <= 98; i++)
+	{
+		if (n === 98)
+		{
+			printf("\n");
+		}
+		else
+		{
+			printf("%d, ", n);
+		}
+	}
+	return (0);
+}
