96afb7b3990ba59de7fcbff81eeb6b0ed366c386
Chidimma Ofodum <chidimmamofodum@gmail.com>
2023-04-25 17:29:09 -0700

    returns last digit of a number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_last_digit - Entry point
+ * Description: prints last digit of a numbber
+ * @i: integer
+ * Return: 0-9
+ */
+
+int print_last_digit(int i)
+{
+	return (i % 10);
+}
+
