49460368e7941cde7540007dfa416db0f3cd0989
Chidimma Ofodum <chidimmamofodum@gmail.com>
2023-04-27 14:04:01 -0700

    multiplies two numbbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - Entry point
+ * @a: input 1
+ * @b: input 2
+ * Description: a function that multiplies two numbers
+ * Return: multiplied output
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
