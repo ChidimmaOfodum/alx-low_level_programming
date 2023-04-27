ea0aba9b8945b7379a2d05c7446cfa518c9c92f8
Chidimma Ofodum <chidimmamofodum@gmail.com>
2023-04-27 13:30:47 -0700

    lower or upper case function

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isupper: Entry point
+ * @c: input parameter
+ * Description: A function that checks if a letter is upper or lower
+ * Return : 0 or 1
+ */
+
+int _isupper(int c)
+{
+	if (c >= 97 && c <= 90)
+	{
+		return (0);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+readme for more fucntion nested loops
