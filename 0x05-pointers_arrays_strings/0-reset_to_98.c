79771e354dd093fd97e6624a851731820767794e
Chidimma Ofodum <chidimmamofodum@gmail.com>
2023-05-03 14:11:55 -0700

    updating the value of a pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * reset_to_98 - resets a number by changing the value of its pointer
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+A readme for arrays and pointers
