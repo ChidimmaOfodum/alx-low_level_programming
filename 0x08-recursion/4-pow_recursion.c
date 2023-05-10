70f710dd8e3759d5900b5262a4d5f3f49cdc233e
Chidimma Ofodum <chidimmamofodum@gmail.com>
2023-05-10 08:18:58 -0700

    calculates the power of a given number recursively

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _pow_recursion - returns a given power of a given number
+ * @x: base
+ * @y: exponent
+ * Return: 0, -1 or the power
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x, y - 1));
+}
