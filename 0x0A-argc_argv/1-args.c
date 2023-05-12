e0541b0a3d98d643238cc65b5706f82d98427da4
Chidimma Ofodum <chidimmamofodum@gmail.com>
2023-05-12 15:49:04 -0700

    prints argument count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of argument passed to it
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 always
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d \n", argc);
+	(void)argv;
+	return (0);
+}
