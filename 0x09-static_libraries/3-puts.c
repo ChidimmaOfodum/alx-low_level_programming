7ff26e83261989f6491fa55cd102cc743369e39f
Chidimma Ofodum <chidimmamofodum@gmail.com>
2023-05-12 13:33:02 -0700

    creating static libraries

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,4 +1,15 @@
-void _puts(char *s)
-{
-}
+#include "main.h"
+/**
+ * _puts - prints characters to stdout
+ * @str: characters to be printed
+ */
 
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
