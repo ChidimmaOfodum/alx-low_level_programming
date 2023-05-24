327c1fe8e1a5c9be5a17e1a524c4b2a0b3dac638
Chidimma Ofodum <chidimmamofodum@gmail.com>
2023-05-24 10:33:34 -0700

    a function that prints a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_name - a function that prints a name
+ * @name: name of the person
+ * @f: pointer to function
+ * Return: nothing
+ *
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
