762597aa54e4431e1e3a38ca64579084fff83942
Chidimma Ofodum <chidimmamofodum@gmail.com>
2023-06-13 05:51:10 -0700

    error fix-a pointer to a new memory containing a string copy

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,24 +1,28 @@
 #include "main.h"
 #include <stdlib.h>
+/**
+ * _strdup - returns a pointer to a string copy
+ * @str: string to be copied
+ * Return: pointer to new memory or null
+ */
 
 char *_strdup(char *str)
-{	
-	int len;
+{
+	int len = 0;
 	int i;
 	char *ptr;
 
 	if (str == NULL)
 		return (NULL);
-	while (*str != '\0')
+	while (str[len] != '\0')
 	{
 		len++;
-		str++;
 	}
-	ptr = malloc(len);
+	ptr = malloc(len + 1);
 	if (ptr == NULL)
 		return (NULL);
 
-	for (i = len - 1; i <= 0; i--)
+	for (i = 0; i < len; i++)
 	{
 		ptr[i] = str[i];
 	}
