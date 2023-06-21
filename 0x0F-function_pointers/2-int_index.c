7eb2bbae324c0c33fb29e6f508e84254bf31dd04
Chidimma Ofodum <chidimmamofodum@gmail.com>
2023-06-21 14:37:12 -0700

    error fix: searches for an integer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,11 +12,15 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i;
 	int result;
 
+	if (array == NULL || cmp == NULL || size <= 0)
+		return (-1);
+
 	for (i = 0; i < size; i++)
 	{
 		result = cmp(array[i]);
 		if (result > 0)
 			return (i);
 	}
-	return (0);
+
+	return (-1);
 }
