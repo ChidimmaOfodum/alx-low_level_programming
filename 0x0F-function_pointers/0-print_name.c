1cf1dc4126b5572d0dea69dc9ba6866d3540777d
Chidimma Ofodum <chidimmamofodum@gmail.com>
2023-06-21 12:15:19 -0700

    error fix

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,7 +4,7 @@
 /**
  * print_name - calls a function with its pointer
  * @name: name to be printed
- * f: function pointer
+ * @f: function pointer
  * Return: void
  */
 
@@ -12,5 +12,6 @@ void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
 		return;
+
 	f(name);
 }
