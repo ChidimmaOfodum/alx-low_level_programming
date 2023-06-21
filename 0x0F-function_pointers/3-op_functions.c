9dbd5cf8c69e40d32d00fa91bd5ba205b01554aa
Chidimma Ofodum <chidimmamofodum@gmail.com>
2023-06-21 14:28:12 -0700

    error fix: array iterator

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds two numbers
+ * @a: first number
+ * @b: second number
+ * Return: result(int)
+ */
+
+int op_add(int a, int b)
+{
+}
+
+/**
+ * op_sub - adds two numbers
+ * @a: first number
+ * @b: second number
+ * Return: result(int)
+ */
+int op_sub(int a, int b)
+{
+
+}
+
+/**
+ * op_mul - adds two numbers
+ * @a: first number
+ * @b: second number
+ * Return: result(int)
+ */
+int op_mul(int a, int b)
+{
+
+}
+
+/**
+ * op_div - adds two numbers
+ * @a: first number
+ * @b: second number
+ * Return: result(int)
+ */
+int op_div(int a, int b)
+{
+
+}
+/**
+ * op_mod - adds two numbers
+ * @a: first number
+ * @b: second number
+ * Return: result(int)
+ */
+int op_mod(int a, int b)
+{
+
+}
+
