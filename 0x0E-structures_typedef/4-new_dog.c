43891b22fbcf9fd07913838513a5dfc5cba0733d
Chidimma Ofodum <chidimmamofodum@gmail.com>
2023-06-24 07:20:42 -0700

    error fix- creating a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
+#include <string.h>
 /**
  * new_dog - creates a new dog
  * @name: name of the dog
@@ -11,14 +12,22 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *my_dog;
+	char *nameCpy = malloc(strlen(name) + 1);
+	char *ownerCpy = malloc(strlen(owner) + 1);
 
 	my_dog = malloc(sizeof(dog_t));
+	strcpy(nameCpy, name);
+	strcpy(ownerCpy, owner);
 	if (!my_dog)
+	{
 		return (NULL);
-	my_dog->name = name;
+		free(my_dog);
+		free(nameCpy);
+		free(ownerCpy);
+	}
+	my_dog->name = nameCpy;
 	my_dog->age = age;
-	my_dog->owner = owner;
+	my_dog->owner = ownerCpy;
 
 	return (my_dog);
-	free(my_dog);
 }
