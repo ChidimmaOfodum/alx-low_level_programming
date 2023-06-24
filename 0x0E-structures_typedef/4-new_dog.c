f57f30fa6c974ca7484483292727d6674c729cd3
Chidimma Ofodum <chidimmamofodum@gmail.com>
2023-06-24 07:02:33 -0700

    creating a new struct

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,24 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * new_dog - creates a new dog
+ * @name: name of the dog
+ * @age: age of dog
+ * @owner: dog owner
+ * Return: struct pointer
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *my_dog;
+
+	my_dog = malloc(sizeof(dog_t));
+	if (!my_dog)
+		return (NULL);
+	my_dog->name = name;
+	my_dog->age = age;
+	my_dog->owner = owner;
+
+	return (my_dog);
+	free(my_dog);
+}
