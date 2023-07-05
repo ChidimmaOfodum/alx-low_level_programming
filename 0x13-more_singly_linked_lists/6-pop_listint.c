e43ae036d1f05d529853a51835ddd978ac579822
Chidimma Ofodum <chidimmamofodum@gmail.com>
2023-07-05 16:30:53 -0700

    poo_list

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * pop_list - deletes a head list
+ * @head: head node
+ * Return: deleted node
+ *
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *current;
+	int n;
+
+	current = *head;
+	n = (*head)->n;
+	*head = (*head)->next;
+	free(current);
+
+	return (n);
+
+}
