2b0ee0c6adef092922c7a082d032da662b3534b6
Chidimma Ofodum <chidimmamofodum@gmail.com>
2023-07-05 07:53:48 -0700

    betty documentation

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,12 +1,21 @@
 #include "lists.h"
+/**
+ * print_listint - prints a elements of a list
+ * @h: head node
+ * Return: number of elements
+ *
+ */
+
 size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
+	const listint_t *current;
 
-	while (h)
+	current = h;
+	while (current)
 	{
-		printf("%d\n", h->n);
-		h = h->next;
+		printf("%d\n", current->n);
+		current = current->next;
 		count++;
 	}
 	return (count);
