137a88bad44b06a504167c059f13f93f19358d4a
Chidimma Ofodum <chidimmamofodum@gmail.com>
2023-07-05 08:44:02 -0700

    free a node list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_listint - frees a node list
+ * head: head node
+ * Return: void
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
