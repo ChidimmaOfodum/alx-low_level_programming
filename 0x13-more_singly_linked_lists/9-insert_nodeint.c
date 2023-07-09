e4ba1068910bb324857c2dcd1a2e37b77138b869
Chidimma Ofodum <chidimmamofodum@gmail.com>
2023-07-09 15:31:42 -0700

    inserting element at the end

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,11 +9,12 @@ if it is not possible to add the new node at index idx, do not add the new node 
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *current, *newnode, *temp;
+	listint_t *current1, *current, *newnode, *temp;
 	unsigned int i = 0;
+	unsigned int j = 0;
 
 	newnode = malloc(sizeof(listint_t));
-	current = *head;
+	current = current1 = *head;
 	
 	if (!newnode || idx < i || (head == NULL))
 		return (NULL);
@@ -24,7 +25,27 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = newnode;
 		return (newnode);
 	}
-		
+	
+	while (current1 != NULL)
+	{
+		if (current1->next == NULL)
+			break;
+		current1 = current1->next;
+		j++;
+	}
+	
+	if (j + 1 == idx)
+	{
+		current1->next = newnode;
+		newnode->next = NULL;
+		newnode->n = n;
+		return (newnode);
+	}
+	printf("Number of nodes is %d\n", j);
+
+
+
+
 	while (current != NULL)
 	{
 		if (i == idx)
@@ -41,6 +62,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			current = current->next;
 		}
 	}
+
 
 	return (NULL);
 }
