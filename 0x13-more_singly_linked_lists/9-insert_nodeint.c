5f8a371fe2a5129d2e3de9d8c2f4a2205f3053b4
Chidimma Ofodum <chidimmamofodum@gmail.com>
2023-07-09 14:49:53 -0700

    inserting node at index final

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+ * Prototype: listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
+where idx is the index of the list where the new node should be added. Index starts at 0
+Returns: the address of the new node, or NULL if it failed
+if it is not possible to add the new node at index idx, do not add the new node and return NULL
+*/
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *current, *newnode, *temp;
+	unsigned int i = 0;
+
+	newnode = malloc(sizeof(listint_t));
+	current = *head;
+	
+	if (!newnode || idx < i || (current == NULL))
+		return (NULL);
+	while (current != NULL)
+	{
+		if (i == idx)
+		{
+			temp->next = newnode;
+			newnode->next = current;
+			newnode->n = n;
+			return (newnode);
+		}
+		else
+		{
+			i++;
+			temp = current;
+			current = current->next;
+		}
+	}
+
+	return (NULL);
+}
