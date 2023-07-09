5d07e6e113873c6f1474840ed7e6d4451fda90dd
Chidimma Ofodum <chidimmamofodum@gmail.com>
2023-07-09 15:33:27 -0700

    inserting element at the end

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -41,10 +41,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		newnode->n = n;
 		return (newnode);
 	}
-	printf("Number of nodes is %d\n", j);
-
-
-
 
 	while (current != NULL)
 	{
