60eef5723334f6106f6c764b3d5cdc2bc03c6afb
Chidimma Ofodum <chidimmamofodum@gmail.com>
2023-07-14 10:20:32 -0700

    setting file to write only

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fdo = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	fdo = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 
 	while (text_content[len])
 	{
