4f23b7314b5d008827ad3faab25b7368d7d742d0
Chidimma Ofodum <chidimmamofodum@gmail.com>
2023-08-17 16:20:58 -0700

    accounting for an edge case - adding at index 0

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -20,6 +20,13 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	while (current != NULL)
 	{
+		if (idx == 0)
+		{
+			new_node->next = current;
+			new_node->prev = NULL;
+			*h = new_node;
+			return (new_node);
+		}
 		if (count == idx - 1)
 		{
 			new_node->next = current->next;
