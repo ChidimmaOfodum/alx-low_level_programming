d8ff40014573597374d1b9c3ad6ce5d0cfc14399
Chidimma Ofodum <chidimmamofodum@gmail.com>
2023-08-17 16:35:49 -0700

    not adding at invalid index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -27,7 +27,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			*h = new_node;
 			return (new_node);
 		}
-		if (count == idx - 1)
+		if (count == idx - 1 && current->next)
 		{
 			new_node->next = current->next;
 			current->next = new_node;
