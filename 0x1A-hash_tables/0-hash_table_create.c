ce6cd441367af06b283b84403ad2ec5c0a5dfe63
Chidimma Ofodum <chidimmamofodum@gmail.com>
2023-09-08 06:22:09 -0700

    a function that creates a hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,21 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - a function that creates a hash table
+ * @size: size of hash table
+ * Return: mem location of table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_node_t *array = malloc(sizeof(size));
+	hash_table_t *hash = malloc(sizeof(hash_table_t));
+
+	if (!hash)
+		return (NULL);
+
+	hash->size = size;
+	hash->array = &array;
+
+	return (hash);
+}
