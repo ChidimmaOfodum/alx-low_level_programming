d737c26002eb01fe80ec24a51d93983c4c330031
Chidimma Ofodum <chidimmamofodum@gmail.com>
2023-09-08 10:09:22 -0700

    initializing hash buckets to null

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,11 +8,15 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	unsigned long int i;
 	hash_node_t **array = malloc(sizeof(hash_node_t) * size);
 	hash_table_t *hash = malloc(sizeof(hash_table_t));
 
 	if (!hash || !array)
 		return (NULL);
+
+	for (i = 0; i < size; i++)
+		array[i] = NULL;
 
 	hash->size = size;
 	hash->array = array;
