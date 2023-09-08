4e6dc4126cc95b94c6e23b90591001da647c06f6
Chidimma Ofodum <chidimmamofodum@gmail.com>
2023-09-08 13:56:49 -0700

    hot fix

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,25 +9,22 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i;
-	hash_node_t **array;
 	hash_table_t *hash = malloc(sizeof(hash_table_t));
-	
+
 	if (!hash)
 		return (NULL);
-	
-	array = malloc(sizeof(hash_node_t) * size);
-	if (!array)
+
+	hash->array = malloc(sizeof(hash_node_t) * size);
+	if (!hash->array)
 	{
 		free(hash);
 		return (NULL);
 	}
+
 	for (i = 0; i < size; i++)
-		array[i] = NULL;
+		hash->array[i] = NULL;
 
 	hash->size = size;
-	hash->array = array;
-
-
 
 	return (hash);
 }
