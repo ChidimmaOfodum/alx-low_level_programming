b83800c24b6f1fa9262f193f68352e02245804f7
Chidimma Ofodum <chidimmamofodum@gmail.com>
2023-09-08 08:51:42 -0700

    declaring array as a double pointer

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,14 +8,14 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_node_t *array = malloc(sizeof(hash_node_t) * size);
+	hash_node_t **array = malloc(sizeof(hash_node_t) * size);
 	hash_table_t *hash = malloc(sizeof(hash_table_t));
 
 	if (!hash || !array)
 		return (NULL);
 
 	hash->size = size;
-	hash->array = &array;
+	hash->array = array;
 
 	return (hash);
 }
