1051a3c70c9dcc08648a3fabcbd5b145b10b868a
Chidimma Ofodum <chidimmamofodum@gmail.com>
2023-12-13 12:55:31 -0500

    Update 0-linear.c-betty

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -9,17 +9,17 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-    size_t i = 0;
+        size_t i = 0;
 
-    if (!array)
-        return (-1);
-    for (i = 0; i < size; i++)
-    {
-        printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-        if (array[i] == value)
+        if (!array)
+                return (-1);
+        for (i = 0; i < size; i++)
         {
-            return (i);
+                printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+                if (array[i] == value)
+                {
+                        return (i);
+                }
         }
-    }
-    return (-1);
+        return (-1);
 }
